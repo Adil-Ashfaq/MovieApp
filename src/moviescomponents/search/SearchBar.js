247c33ad6c7b60e7fdb183d3f9368171ded5ea9b
M-ADIL-ASHFAQ\ADIL <adil.ashfaq17@gmail.com>
2022-09-12 15:31:47 +0500

    first commit

---
+++ src/moviescomponents/search/SearchBar.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Button, InputGroup, Form } from "react-bootstrap";
+
+function SearchBar({ searchInput, handleSearchInputChange, handleClearInput }) {
+  return (
+    <InputGroup>
+      <Form.Control
+        value={searchInput}
+        onChange={handleSearchInputChange}
+        placeholder="Search movie by title"
+        aria-label="Search Movie"
+      />
+      {searchInput ? (
+        <Button
+          variant="outline-secondary"
+          id="btnClear"
+          onClick={handleClearInput}
+        >
+          clear
+        </Button>
+      ) : null}
+    </InputGroup>
+  );
+}
+
+export default SearchBar;
